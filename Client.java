9d701c416b4ea8c24a9f430040ae5e566ff8774a
ndemanou steve <ndemanousteve@gmail.com>
2019-10-14 10:00:33 +0200

    Partage initial du projet

---
+++ Client.java
@@ -0,0 +1,56 @@
+
+/**
+ * Décrivez votre classe Client ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+public class Client extends Humain
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    protected String boissonFavorite;
+    protected String boissonDeSecours;
+    protected int niveauAlcoolemie;
+    
+    public String getBoissonFavorite()
+    {
+        return this.boissonFavorite;
+    }
+    public void setBoissonFavorite(String boisson)
+    {
+        this.boissonFavorite= boisson;
+    }
+    
+     public String getBoissonDeSecours()
+    {
+        return this.boissonDeSecours;
+    }
+    public void setBoissonDeSecours(String boisson)
+    {
+        this.boissonDeSecours= boisson;
+    }
+    /**
+     * Constructeur d'objets de classe Client
+     */
+    public Client()
+    {
+        // initialisation des variables d'instance
+       
+    }
+
+    /**
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    public void getDrinks() // se faire offrir un verre
+    {
+        // Insérez votre code ici
+      
+    }
+    public void otherPresentation() // se présenter autrement 
+    {
+        
+    }
+}
