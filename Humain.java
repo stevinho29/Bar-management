9e4b0787641f6378418adb835c1ad25d1f9ddd41
Josias SEMANOU <josias.semanou@hei.yncrea.fr>
2019-10-20 17:40:39 +0100

    Humain modifié

--- Humain.java
+++ Humain.java
@@ -5,57 +5,73 @@
  * @author (Steve ndemanou)
  * @version (13/10/2019)
  */
-public abstract class Humain
-{
-    // variables d'instance - remplacez l'exemple qui suit par le vôtre
-
+public abstract class Humain {
+    
     protected String prenom;
     protected String surnom;
-    protected int porteMonnaie;
-    protected int cotePopularite; 
+    protected float porteMonnaie;
+    protected int cotePopularite;
     protected String criSignificatif;
-    
-    public String getPrenom()
-    {
-        return this.prenom;
-    }
-    
-    /**
-     * Constructeur d'objets de classe Humain
-     */
-    public Humain()
-    {
-        // initialisation des variables d'instance
-       
-        
+
+    public Humain(String prenom, String surnom, int porteMonnaie, int cotePopularite, String criSignificatif) {
+        this.prenom = prenom;
+        this.surnom = surnom;
+        this.porteMonnaie = porteMonnaie;
+        this.cotePopularite = cotePopularite;
+        this.criSignificatif = criSignificatif;
     }
 
-    /**
-     * Un exemple de méthode - remplacez ce commentaire par le vôtre
-     *
-     * @param  y   le paramètre de la méthode
-     * @return     la somme de x et de y
-     */
-    public void  parler() //discuter avec des gens 
-    {
-        // Insérez votre code ici
-        return ;
+    public String getPrenom() {
+        return prenom;
     }
-    public void boire() // boire un verre 
-    {
-        return ;
+
+    public String getSurnom() {
+        return surnom;
     }
-    public void payer() // payer sa note
-    {
-        return ;
+
+    public float getPorteMonnaie() {
+        return porteMonnaie;
     }
-    public void offerDrinks() // offrir un verre à quelqu'un
-    {
-        return ;
+
+    public int getCotePopularite() {
+        return cotePopularite;
     }
-    public void presentMyself()
-    {
-        
-        return ;
+
+    public String getCriSignificatif() {
+        return criSignificatif;
+    }
+
+    public void setSurnom(String surnom) {
+        this.surnom = surnom;
+    }
+
+    public void setPorteMonnaie(int porteMonnaie) {
+        this.porteMonnaie = porteMonnaie;
+    }
+
+    public void setCotePopularite(int cotePopularite) {
+        this.cotePopularite = cotePopularite;
+    }
+
+    public void setCriSignificatif(String criSignificatif) {
+        this.criSignificatif = criSignificatif;
+    }
+    
+    public void  parler(String message) { //discuter avec des gens 
+        System.out.println(message);
+    }
+    
+    public void boire() { // boire un verre
+    }
+    
+    public void payer() {// payer sa note
+    }
+    
+    public void offerDrinks() {// offrir un verre à quelqu'un
+    }
+    
+    public void presentMyself() {
+        System.out.println("Je suis " + prenom + " aussi appelé " + surnom + ".");
+        System.out.println("Il me reste " + porteMonnaie + " euros en poche. Et je crie souvent '" + criSignificatif + "' ! ");
     }
 }
